7c6dafc385517a3dad1426da86a1b15e2f965773
NguyenHuuDanh <113421911+DanhNguyen02@users.noreply.github.com>
2023-03-10 20:22:08 +0700

    Create History module

---
+++ mern/client/src/pages/History/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function History() {
+  return <div>History</div>;
+}
+export default History;
--- mern/client/src/routes/index.js
+++ mern/client/src/routes/index.js
@@ -3,11 +3,13 @@ import { DefaultLayout } from "../components/Layouts";
 
 // Import Route Pages
 import Dashboard from "../pages/Dashboard";
+import History from "../pages/History";
 
 // Not Required Login
 // layout:null --> No Layout
 const publicRoutes = [
   { path: "/", component: Dashboard, layout: DefaultLayout },
+  { path: "/history", component: History, layout: DefaultLayout }
 ];
 
 //Require Login
