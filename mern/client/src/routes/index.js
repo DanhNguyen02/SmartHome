88b12bf3e412ec80e93ccdd0d86a0b2a181bd853
VuTran2601 <nguyenvutran2601@gmail.com>
2023-03-12 22:47:06 +0700

    First commit

--- mern/client/src/routes/index.js
+++ mern/client/src/routes/index.js
@@ -3,11 +3,17 @@ import { DefaultLayout } from "../components/Layouts";
 
 // Import Route Pages
 import Dashboard from "../pages/Dashboard";
+import Login from "../pages/User/Login";
+import Register from "../pages/User/Register"
+import Profile from "../pages/User/Profile"
 
 // Not Required Login
 // layout:null --> No Layout
 const publicRoutes = [
   { path: "/", component: Dashboard, layout: DefaultLayout },
+  { path: "/login", component: Login, layout: null },
+  { path: "/register", component: Register, layout: null },
+  { path: "/profile", component: Profile, layout: DefaultLayout },
 ];
 
 //Require Login
