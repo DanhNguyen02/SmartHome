f699d05479dd6ae4d971dedf2d2216c733bf7cbf
VuTran2601 <nguyenvutran2601@gmail.com>
2023-03-20 13:12:19 +0700

    Updates

--- mern/client/src/pages/User/Profile/index.js
+++ mern/client/src/pages/User/Profile/index.js
@@ -33,7 +33,7 @@ const ProfilePicture = () => {
                         width: '100%',
                         height: '100%',
                         objectFit: 'cover',
-                        borderRadius: 50,
+                        borderRadius: 180,
                         border: '1px solid black', }}/>
             </Box>
             <Button
